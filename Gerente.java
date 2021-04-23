573f70f73e42bbfb6d8557184dda06d057a38f56
Eladio Leal <eladiolink1@gmail.com>
2021-04-23 14:39:28 -0300

    Mudanças no Gerente

--- Gerente.java
+++ Gerente.java
@@ -18,135 +18,181 @@ public class Gerente{
 
     public void CriarContaPoupanca(String nome){
         
-         int indice=BuscarPessoa(nome);
-        
-         Scanner cliente=new Scanner(System.in);
-         System.out.print("Digite o saldo Inicial da Conta: ");
-         Double saldo= cliente.nextDouble();
+        for( Banco conta : this.contas ){
+         
+            if( conta.getNomeCliente().equals(nome) ){
+                Scanner cliente=new Scanner(System.in);
+                System.out.print("Digite o saldo Inicial da Conta: ");
+                Double saldo= cliente.nextDouble();
 
-         System.out.print("Digite o dia do seu Aniversário: ");
-         int dia= cliente.nextInt();
+                System.out.print("Digite o dia do seu Aniversário: ");
+                int dia= cliente.nextInt();
  
- 
-        this.contas.get(indice).CriarContaPoupanca(this.numeroDeConta,1776,"Nubank",saldo,dia,0.05);
+                conta.CriarContaPoupanca(this.numeroDeConta,1776,"Nubank",saldo,dia,0.05);
 
-        this.numeroDeConta++;
+                this.numeroDeConta++;
+            }
+           
+         }  
     
     }
 
     public void CriarContaCorrente(String nome){
-        int indice=BuscarPessoa(nome);
-       
-        Scanner cliente=new Scanner(System.in);
-        System.out.println("Digite o saldo Inicial da Conta: ");
-        Double saldo= cliente.nextDouble();
-
-        System.out.println("Digite o valor do seu cheque especial");
-        Double cheque= cliente.nextDouble(); 
-
-       this.contas.get(indice).CriarContaCorrente(this.numeroDeConta,1881,"Nubank",saldo,cheque);
-
-       this.numeroDeConta++;
+        
+          
+        for( Banco conta : this.contas ){
+         
+            if( conta.getNomeCliente().equals(nome) ){
+                Scanner cliente=new Scanner(System.in);
+                System.out.println("Digite o saldo Inicial da Conta: ");
+                Double saldo= cliente.nextDouble();
+        
+                System.out.println("Digite o valor do seu cheque especial");
+                Double cheque= cliente.nextDouble(); 
+        
+                conta.CriarContaCorrente(this.numeroDeConta,1881,"Nubank",saldo,cheque);
+        
+               this.numeroDeConta++;
+            }
+           
+         }
    
    }
 
    public void CriarContaSalario(String nome){
-    int indice=BuscarPessoa(nome);
    
-    Scanner cliente=new Scanner(System.in);
-    System.out.print("Digite o saldo Inicial da Conta: ");
-    Double saldo= cliente.nextDouble();    
+    for( Banco conta : this.contas ){
+         
+        if( conta.getNomeCliente().equals(nome) ){
+            Scanner cliente=new Scanner(System.in);
+            System.out.print("Digite o saldo Inicial da Conta: ");
+            Double saldo= cliente.nextDouble();    
 
-    System.out.println("Digite a quantidades de saques mensais");
-    int saques= cliente.nextInt();
+            System.out.println("Digite a quantidades de saques mensais");
+            int saques= cliente.nextInt();
 
-   this.contas.get(indice).CriarContaSalario(this.numeroDeConta,3232,"Nubank",saldo,saques);
-   this.numeroDeConta++;
-
+            conta.CriarContaSalario(this.numeroDeConta,3232,"Nubank",saldo,saques);
+            this.numeroDeConta++;
+        }
+       
+     }
    }
 
    public void SacarPoupanca(String nome){
-    int indice=BuscarPessoa(nome);
+    
      
-    System.out.println(indice);
-   
-    Scanner cliente=new Scanner(System.in);
-    System.out.print("Digite o quanto quer Sacar: ");
-    Double valor= cliente.nextDouble();
-    this.contas.get(indice).sacarContaPoupanca(valor);
+     for( Banco conta : this.contas ){
+         
+        if( conta.getNomeCliente().equals(nome) ){
+            System.out.println(conta.getNomeCliente()+" - "+conta.ContaPoupanca.getSaldo());
+
+           Scanner cliente=new Scanner(System.in);
+           System.out.print("Digite o quanto quer Sacar: ");
+           Double valor= cliente.nextDouble();
+          conta.sacarContaPoupanca(valor);
+        }
+       
+
+     }
 
    }
 
    public void SacarCorrente(String nome){
-    int indice=BuscarPessoa(nome);
+   
+    for( Banco conta : this.contas ){
+         
+        if( conta.getNomeCliente().equals(nome) ){
+
+            Scanner cliente=new Scanner(System.in);
+            System.out.print("Digite o quanto quer Sacar: ");
+            Double valor= cliente.nextDouble();
+            conta.SacarContaCorrente(valor);   
+        }
+       
+
+     }
     
-    Scanner cliente=new Scanner(System.in);
-    System.out.print("Digite o quanto quer Sacar: ");
-    Double valor= cliente.nextDouble();
-    this.contas.get(indice).SacarContaCorrente(valor);   
 
    }
 
    public void SacarSalario(String nome){
-    int indice=BuscarPessoa(nome);
-    Scanner cliente=new Scanner(System.in);
-    System.out.print("Digite o quanto quer Sacar: ");
-    Double valor= cliente.nextDouble();
-    this.contas.get(indice).SacarContaSalario(valor);
     
+    for( Banco conta : this.contas ){
+         
+        if( conta.getNomeCliente().equals(nome) ){
+        
+            Scanner cliente=new Scanner(System.in);
+            System.out.print("Digite o quanto quer Sacar: ");
+            Double valor= cliente.nextDouble();
+            conta.SacarContaSalario(valor);
+        }
+       
+
+     }
 
    }
 
-    private int BuscarPessoa(String nome){
-             
-         for(int i=0 ;i < this.contas.size() ; i++ ){
-                       
-             if(this.contas.get(i).getNomeCliente().equals(nome)){
-                  return i;
-             }
-        }
-        return-1;
-    }
-
 
     public void DepositarPoupanca(String nome){
-        int indice=BuscarPessoa(nome);
-     
-    System.out.println(indice);
-   
-    Scanner cliente=new Scanner(System.in);
-    System.out.print("Digite o valor de deposito: ");
-    Double valor= cliente.nextDouble();
-    this.contas.get(indice).DepositarContaPoupanca(valor);
-
-
+        for( Banco conta : this.contas ){
+         
+            if( conta.getNomeCliente().equals(nome) ){
+    
+                Scanner cliente=new Scanner(System.in);
+                System.out.print("Digite o valor de deposito: ");
+                Double valor= cliente.nextDouble();
+                conta.DepositarContaPoupanca(valor);
+            }
+           
+    
+         }
+       
     }
+
+    
     public void DepositarSalario(String nome){
-        int indice=BuscarPessoa(nome);
-     
-    System.out.println(indice);
-   
-    Scanner cliente=new Scanner(System.in);
-    System.out.print("Digite o valor de deposito: ");
-    Double valor= cliente.nextDouble();
-    this.contas.get(indice).DepositarContaSalario(valor);
-
-
+       
+        for( Banco conta : this.contas ){
+         
+            if( conta.getNomeCliente().equals(nome) ){
+                Scanner cliente=new Scanner(System.in);
+                System.out.print("Digite o valor de deposito: ");
+                Double valor= cliente.nextDouble();
+                conta.DepositarContaSalario(valor);
+            }
+         }
     }
+
     public void DepositarCorrente(String nome){
-        int indice=BuscarPessoa(nome);
-     
-    System.out.println(indice);
-   
-    Scanner cliente=new Scanner(System.in);
-    System.out.print("Digite o valor de deposito: ");
-    Double valor= cliente.nextDouble();
-    this.contas.get(indice).DepositarContaCorrente(valor);
 
-
+        for( Banco conta : this.contas ){
+         
+            if( conta.getNomeCliente().equals(nome) ){
+                Scanner cliente=new Scanner(System.in);
+                System.out.print("Digite o valor de deposito: ");
+                Double valor= cliente.nextDouble();
+                conta.DepositarContaCorrente(valor);
+            }
+         }
     }
 
-   
+    public void Saldo(){
+        
+        Scanner cliente=new Scanner(System.in);
+                System.out.print("Digite o seu nome: ");
+                String nome= cliente.nextLine();
+
+        for( Banco conta : this.contas ){
+         
+            if( conta.getNomeCliente().equals(nome) ){
+                System.out.println(conta.getNomeCliente());
+
+                System.out.println(conta.Saldo());
+        
+            }
+         }
+
+    }
 
     
 }
